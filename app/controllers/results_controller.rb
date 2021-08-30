0d63581a9ec639a0f4c546f82f873db879a9615e
AndreyTes <andrey_tes2@mail.ru>
2021-08-30 14:41:49 +0300

    Create request

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,6 @@
 class ResultsController < ApplicationController
   def index
-    
+    @search_results = Procedure.where('title ILIKE ?', "%#{params[:query]}%")
+                               .order("CASE WHEN (title ILIKE '#{params[:query]}%') THEN 0 ELSE 1 END")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Procedure.create([{title: 'Biopsy test'}, {title: 'Blood test'}, {title: 'Stool test'}, {title: 'Urinalysis'}, {title: 'Cardiac stress test'}, 
+    {title: 'Electrocardiography'}, {title: 'Electrocorticography'}, {title: 'Electroencephalography'}, {title: 'Electromyography'}, 
+    {title: 'Electroneuronography'}, {title: 'Electronystagmography'}, {title: 'Electrooculography'}, {title: 'Electroretinography'}, 
+    {title: 'Endoluminal capsule monitoring'}, {title: 'Endoscopy'}, {title: 'Colonoscopy'}, {title: 'Colposcopy'}, {title: 'Cystoscopy'}, 
+    {title: 'Gastroscopy'}, {title: 'Laparoscopy'}, {title: 'Laryngoscopy'}, {title: 'Ophthalmoscopy'}, {title: 'Otoscopy'}, {title: 'Sigmoidoscopy'}, 
+    {title: 'Esophageal motility study'}, {title: 'Evoked potential'}, {title: 'Magnetoencephalography'}, {title: 'Medical imaging'}, 
+    {title: 'Aortography'}, {title: 'Cerebral angiography'}, {title: 'Scopy'}, {title: 'Electro'}, {title: 'Graphy'},
+    {title: 'la'}, {title: 'Olala'},
+    {title: 'olao'}, {title: 'oola'},
+    {title: 'lacolato'}, {title: 'okjkghla'}]
+
+
+
+
+)
